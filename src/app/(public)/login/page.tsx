8bec0ee3f5cb5fb9e65f6710915ff581532708d3
mateuslopes92 <mateuslopes92@gmail.com>
2025-11-24 22:22:10 -0300

    Refactor on login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -10,10 +10,6 @@ export default function LoginPage() {
   return (
     <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 items-center mt-20">
       <div className="px-6 py-4">
-        <div className="flex justify-center mx-auto">
-          <Image src="https://merakiui.com/images/logo.svg" alt="Logo" width={100} height={28} />
-        </div>
-
         <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">Welcome</h3>
 
         <p className="mt-1 text-center text-gray-500 dark:text-gray-400">Login or create account</p>
@@ -28,7 +24,7 @@ export default function LoginPage() {
           </div>
 
           <div className="flex items-center mt-4">
-            <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+            <button className="w-full px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
               Sign In
             </button>
           </div>
